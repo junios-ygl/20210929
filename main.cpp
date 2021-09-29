72718c93ad53ddd7f520c635b27f3cb33bdf90c2
junios.ygl <junios.ygl@gmail.com>
2021-09-29 15:02:30 +0900

    총돌처리

--- main.cpp
+++ main.cpp
@@ -6,19 +6,20 @@ using namespace std;
 void Input();
 void Process();
 void Draw();
+void MovePlayer(int XDirection, int YDirection);
 
 //1. 지도를 초기화 한다.
 int Map[10][10] =
 {
 	{1,1,1,1,1,1,1,1,1,1},
+	{1,0,0,0,0,0,1,0,0,1},
+	{1,0,1,0,0,0,1,0,0,1},
+	{1,0,1,0,0,0,1,0,0,1},
+	{1,0,1,0,0,0,1,0,0,1},
 	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,1,0,0,1,0,1},
+	{1,0,0,0,1,0,0,1,0,1},
+	{1,0,0,0,1,0,0,1,0,1},
 	{1,1,1,1,1,1,1,1,1,1}
 };
 
@@ -26,7 +27,7 @@ int PlayerX = 1;
 int PlayerY = 1;
 
 bool bIsRunning = true;
-char Key;
+int Key;
 
 
 int main()
@@ -44,6 +45,11 @@ int main()
 void Input()
 {
 	Key = _getch();
+	if (Key == 0x00 || Key == 0xE0)
+	{
+		// 확장키의 경우 키를 하나더 입력 받는다.
+		Key = _getch();
+	}
 }
 
 void Process()
@@ -54,28 +60,28 @@ void Process()
 		case 'W':
 		case 'w':
 		{
-			PlayerY--;
+			MovePlayer(0, -1);
 			break;
 		}
 		//down
 		case 'S':
 		case 's':
 		{
-			PlayerY++;
+			MovePlayer(0, 1);
 			break;
 		}
 		//left
 		case 'A':
 		case 'a':
 		{
-			PlayerX--;
+			MovePlayer(-1, 0);
 			break;
 		}
 		//right
 		case 'D':
 		case 'd':
 		{
-			PlayerX++;
+			MovePlayer(1, 0);
 			break;
 		}
 
@@ -102,16 +108,31 @@ void Draw()
 			{
 				cout << "P" << " ";
 			}
-			else if (Map[X][Y] == 0)
+			else if (Map[Y][X] == 0)
 			{
 				cout << " " << " ";
 			}
-			else if (Map[X][Y] == 1)
+			else if (Map[Y][X] == 1)
 			{
 				cout << "X" << " ";
 			}
 		}
 
 		cout << endl;
+	}
+}
+
+void MovePlayer(int XDirection, int YDirection)
+{
+	//새로 가볼곳 계산
+	int NewPlayerX = PlayerX + XDirection;
+	int NewPlayerY = PlayerY + YDirection;
+
+	//미리 가봄
+	if (Map[NewPlayerY][NewPlayerX] == 0)
+	{
+		//이동
+		PlayerX = NewPlayerX;
+		PlayerY = NewPlayerY;
 	}
 }
